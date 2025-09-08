bc1796d88a725f418de39f6ee7a05f13fcd278b1
batuhan yeltekin <3pixter@gmail.com>
2025-09-07 21:51:23 -0400

    coffee chat disclaimer added

--- app/recruitment/page.tsx
+++ app/recruitment/page.tsx
@@ -212,9 +212,23 @@ export default function RecruitmentPage() {
               Register for Coffee Chats
             </h2>
             <div className="w-20 h-1 bg-cuwmc-secondary mx-auto mb-8"></div>
-            <p className="text-lg text-gray-700 leading-relaxed font-garamond max-w-3xl mx-auto">
+            <p className="text-lg text-gray-700 leading-relaxed font-garamond max-w-3xl mx-auto mb-6">
               Schedule a coffee chat with our executive board members to learn more about CUWMC and get your questions answered. Choose from the available time slots below.
             </p>
+            <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 max-w-4xl mx-auto">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <div className="ml-3">
+                  <p className="text-sm text-yellow-700 font-garamond">
+                    <strong>Important:</strong> Each prospective applicant can sign up for a maximum of 2 coffee chats (each with a different member). Any additional bookings beyond this limit will be automatically cancelled.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
 
           {/* Google Calendar Appointment Schedules */}
